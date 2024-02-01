bb4c78a4bbb3bd54855e4595ca42bd51defc1f4f
unknown <Admin@CSLAB237.CSSERVER.local>
2024-02-01 20:11:06 +0800

    haha

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Hitler
+{
+
+
+
+
+
+    public class Math
+    {
+        public void Add(int num1, int num2)
+        {
+            Console.WriteLine("Addition=  " + (num1 + num2));
+        }
+
+        public void Sub(int num1, int num2)
+        {
+            Console.WriteLine("Subtraction=  " + (num1 - num2));
+        }
+        public void Mul(int num1, int num2)
+        {
+            Console.WriteLine("Multiplication=  " + (num1 * num2));
+        }
+        public void Div(int num1, int num2)
+        {
+            Console.WriteLine("Division=  " + (num1 / num2));
+        }
+
+
+    }
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math meth = new Math();
+
+            int num1, num2;
+
+            Console.WriteLine("Enter Num 1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Num 2:");
+            num2 = int.Parse(Console.ReadLine());
+
+            meth.Add(num1, num2);
+            meth.Sub(num1, num2);
+            meth.Mul(num1, num2);
+            meth.Div(num1, num2);
+
+
+
+        }
+    }
+}
